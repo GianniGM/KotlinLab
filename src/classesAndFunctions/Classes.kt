ed1dbb364ed226d54e4692425e29611094c056d6
GianniGM <giangraziano@gmail.com>
2018-05-31 11:38:25 +0200

    Added Classes sample

---
+++ src/classesAndFunctions/Classes.kt
@@ -0,0 +1,53 @@
+package classesAndFunctions
+
+/*  NOTE:
+ *  data class provides a class with built-in method
+ *  setters and getters
+ *
+ *  toString() => convert in a string
+ *  equals() => used directly by == operator
+ *  hashCode() => provides an hashCode()
+ *  copy() => method to copy an object
+ *  componentN() functions used for destructuring operator
+ *
+ *  further information: https://bit.ly/2L7yxs6 */
+data class User( //constructor
+
+        //fields with getters
+        val userName: String,
+        val age: Int,
+        val gender: String,
+
+        //fields with getters and setters
+        var isOnline: Boolean = false
+)
+
+fun main(args: Array<String>) {
+    val greetingsList = greetings()
+    println(greetingsList)
+}
+
+private fun greetings(): String {
+    //todo: for a better performance of map, filter, sortedBy check Kotlin Sequences
+
+    return generateUsersList()
+            .filter { it.age in 10..23 }
+            .sortedBy { it.age }
+            .map {
+                when (it.gender) {
+                    "female" -> "Hi Lady ${it.userName}"
+                    "male" -> "Hi Man ${it.userName}"
+                    else -> "Hi Dear ${it.userName}"
+                }
+            }
+            //reduce all user values in a string
+            .reduce { acc, s -> "$acc, $s" }
+}
+
+fun generateUsersList() = listOf(
+        User("Larry", 28, "male"),
+        User("Sibo", 37, "male"),
+        User("Lalla", 22, "female"),
+        User("MJ", 20, "female"),
+        User("Harry Potter", 12, "Vingardium Leviosa")
+)
