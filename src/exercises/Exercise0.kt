d5aa510e398f6a765675a8a2e9d3a3094234ec1d
GianniGM <giangraziano@gmail.com>
2018-06-10 17:47:07 +0200

    fixed exercise trackt

--- src/exercises/Exercise0.kt
+++ src/exercises/Exercise0.kt
@@ -1,11 +1,13 @@
 package exercises
 
-fun main(args: Array<String>) {
-    //1. create an infix extensions function power
-    // that calculate a power of an Int
-    // without using pow
+// 1. create an Int infix extensions function "power"
+// without using pow
+infix fun Int.power(a:Int): Int{
 
+}
+
+fun main(args: Array<String>) {
     //2. calculate 5^4
 
-    //2. print the result
+    //3. print the result
 }
