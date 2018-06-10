b530279eb20820a8d934905c51a02f3345747972
GianniGM <giangraziano@gmail.com>
2018-06-10 18:46:45 +0200

    changed exercise 2 track

--- src/exercises/Exercise2.kt
+++ src/exercises/Exercise2.kt
@@ -3,19 +3,20 @@ package exercises
 data class User(var name: String, var surname: String, var age: Int)
 
 fun main(args: Array<String>) {
-    val list = listOf(
+    val usersList = listOf(
             User("Paperina", "Disney", 10),
             User("Black Widow", "Marvel", 23),
             User("Pluto", "Disney", 48),
             User("Harry", "Potter", 11),
+            User("Ginny", "Potter", 10),
             User("James", "Potter", 40),
             User("Thor", "Marvel", 400),
             User("Tick", "Amazon", 52)
     )
 
-    println("PRINT USERNAMES BEGINNING WITH T")
+    println("PRINT NAMES BEGINNING WITH T")
 
     println("PRINT USERS WITH AGE INCLUDING BETWEEN 20 AND 100")
 
-    println("PRINT SURNAMES (AVOID DUPLICATIONS) AGE OF EVEN NUMBER")
+    println("PRINT SURNAMES OF USERS WITH EVEN AGE SORTED ALPHABETICALLY AVOIDING DUPLICATES")
 }
