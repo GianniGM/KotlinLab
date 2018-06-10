18c06f20325a9c615f4cc0a554ee69c0b226136c
Leo Dip <leodip97@gmail.com>
2018-06-10 17:45:26 +0200

    added exercise0 solution

--- src/exercises/Exercise0.kt
+++ src/exercises/Exercise0.kt
@@ -1,11 +1,24 @@
 package exercises
 
+//1. create an infix extensions function "power" that calculate a power of an Int
+infix fun Int.power(a: Int): Int {
+    var result = 1
+    for (any in 1 .. a)
+        result *= this
+    return result
+}
+
 fun main(args: Array<String>) {
-    //1. create an infix extensions function "elevation"
-    // that calculate a power of an Int
-    // without using pow
-
     //2. calculate 5^4
+    val a = 5.power(4)
 
-    //2. print the result
+    //3. print the result
+    println(a)
+}
+
+infix fun Int.power(a: Int): Int {
+    var result = 1
+    for (any in 1 .. a)
+        result *= this
+    return result
 }
