7b36fc4686ff783fb5cf6ebb906491d3b5c77b2a
GianniGM <giangraziano@gmail.com>
2018-06-10 16:15:39 +0200

    fixed typos

--- src/basics/TypesStringsRanges.kt
+++ src/basics/TypesStringsRanges.kt
@@ -2,7 +2,8 @@ package basics
 
 fun main(args: Array<String>) {
     variables()
-    ifThenElse()
+    ifThenElse(5, 9)
+    sum(5,78)
     strings()
     ranges()
 }
@@ -11,7 +12,7 @@ private fun variables() {
 
     // read only variables you can't change a val, just a var
     val age: Int = 28
-    val longType: Long = 23
+    val longType: Long = 23_0000_000 //23_000_000L
     val floatType: Float = 3F
 
     // writable variable you can change it
@@ -22,29 +23,6 @@ private fun variables() {
 
 }
 
-private fun ifThenElse() {
-    val a = 5
-    val b = 90
-
-    // If you want to inizialize a variable to null you must declare it as String?
-    // with question mark
-    var string1:String? = null
-    if (a < b) {
-        string1 = "a is minor than b"
-    } else {
-        string1 = "b is minor than b"
-    }
-    println(string1.toString())
-
-    //Kotlin style
-    val string = if (a < b)
-        "a is minor than b"
-    else
-        "b is minor than b"
-
-    println(string)
-}
-
 private fun variablesSemplified() {
 
     // read only variables you can't change a val, just a var
@@ -53,48 +31,59 @@ private fun variablesSemplified() {
     val floatType = 3F
 
     // writable variable you can change it
-    lateinit var userName: String
-
+    var userName: String = "KotlinUser"
     userName = "GDG Pisa User"
 
-    println("My name is $userName and i have $age years old: $longType and $floatType")
+    println("My name is $userName and i am $age years old: $longType and $floatType")
 }
 
-private fun strings() {
-    val a = 5
-    val b = 10
+private fun ifThenElse(a: Int, b: Int) {
 
+    lateinit var str: String
+    if (a < b)
+        str = "a less than b"
+    else
+        str = "b less than a"
+
+    // val str = if(a < b) "a less than b" else "b less than 2
+
+    println(str)
+}
+
+private fun sum(a: Int, b: Int){
     // you can print variables like this WRONG way...
     println("The sum of" + a + " " + b + "is " + (a + b))
 
     // ...or in this, absolutely better, way
-    println("The better way to sum $a + $b is ${a + b}")
+    println("The better way to sum $a and $b is ${a + b}")
+}
 
+private fun strings() {
     val banana = "banana"
     val strawberry = "strawberry"
     val bananaTwo = "banana"
 
     //you can compare strings just using ==
     println("banana and bananaTwo ${banana == bananaTwo}")
-    println("banana and stawberry ${banana == strawberry}")
+    println("banana and strawberry ${banana == strawberry}")
 }
 
 private fun ranges() {
-    val fruits = 50
+    val fruitsNumber = 50
 
     //range used in if-then-else
-    if (fruits in 1..100)
+    if (fruitsNumber in 1..100)
         println("enough fruits :)")
     else
         println("not enough :(")
 
     //you can use !in operator to check if a value is outside of a range
-    if (fruits !in 60..80)
+    if (fruitsNumber !in 60..80)
         println("fruits number are outside the range")
 
-    when (fruits) {
-        0 -> println("no fruit")
-        in 10..49 -> println("ok :)")
+    when (fruitsNumber) {
+        0 -> println("no fruits")
+        in 1..50 -> println("ok :)")
         50 -> println("fifty fruits are enough")
         else -> println("PLZ STOP!")
     }
