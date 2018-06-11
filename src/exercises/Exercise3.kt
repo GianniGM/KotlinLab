91175da949ac98d02bf0bd36fc9c6d08fc89ce1a
GianniGM <giangraziano@gmail.com>
2018-06-11 21:05:47 +0200

    added exercise 3 tracks

--- src/exercises/Exercise3.kt
+++ src/exercises/Exercise3.kt
@@ -1,5 +1,39 @@
 package exercises
 
+// PART 1: Implement a binary tree
+// A binary tree is made up of nodes which are objects
+// with a left child node, a right child node and a Int value.
+// If a node is missing one of the two children, make that child a null
+
 fun main(args: Array<String>) {
+    // PART 2: Build a binary search tree
+    // Build the following binary tree
+    //              10
+    //           5      15
+    //         2   6  X    18
+
+
+
+    // PART 3: Print a node
+    // Write a function printNode that prints a node's representation
+    // Print the tree as sequence of numbers beginning from the smallest value,
+    // you can do it recursively
+
+
+
+    // (GDG PISA BONUS) PART 4: SUM-TREE
+    // Infix extension function
+
+    // write an infix extension function
+    // that links two nodes with a superNode containing the min value of the child
+
+    // Node(2) minTree Node(3)
+    //
+    //     2
+    //  2     3
 
 }
+
+// todo: you may fing interesting let and with
+// let  https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/let.html
+// with https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/with.html
