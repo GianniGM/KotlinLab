e12414d62fd7a75f4a3d67936f64a453f28e02a2
GianniGM <giangraziano@gmail.com>
2018-06-16 19:34:15 +0200

    updadet exercise 3

--- src/exercises/Exercise3.kt
+++ src/exercises/Exercise3.kt
@@ -1,39 +1,44 @@
 package exercises
 
-// PART 1: Implement a binary tree
+// 1. Implement a binary tree
 // A binary tree is made up of nodes which are objects
 // with a left child node, a right child node and a Int value.
 // If a node is missing one of the two children, make that child a null
 
+
 fun main(args: Array<String>) {
-    // PART 2: Build a binary search tree
-    // Build the following binary tree
+    // 2. Build a binary tree
+    // Using the Node class you built, build the following binary tree
     //              10
     //           5      15
     //         2   6  X    18
 
 
-
     // PART 3: Print a node
-    // Write a function printNode that prints a node's representation
-    // Print the tree as sequence of numbers beginning from the smallest value,
-    // you can do it recursively
+    // Write a function "printNode" that prints an the values in an ordered
+    // binary tree (which means it should print from the lowest value stored to the highest)
+    // (hint: recursion)
+    // Expected output would be 2 5 6 10 15 18
+    // [Write the function in the allotted space on top of the file]
 
 
 
     // (GDG PISA BONUS) PART 4: SUM-TREE
     // Infix extension function
+    // write an infix extension function that takes two Nodes as input and
+    // returns a Node that has:
+    // - the first of the two as left child
+    // - the second of the two as right child
+    // - the minimum value of the two as value
+    // [Write the function in the allotted space on top of the file]
 
-    // write an infix extension function
-    // that links two nodes with a superNode containing the min value of the child
-
-    // Node(2) minTree Node(3)
+    // e.g.: Node(2) minTree Node(3)
     //
     //     2
     //  2     3
 
+
 }
 
-// todo: you may find interesting "let" and "with"
 // let  https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/let.html
 // with https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/with.html
