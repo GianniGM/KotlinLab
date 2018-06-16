7e36be9c90732cb6f49cf0218dea6d42cc38e160
GianniGM <giangraziano@gmail.com>
2018-06-16 20:12:28 +0200

    fixed typos in lambda

--- src/lambdas/lambda.kt
+++ src/lambdas/lambda.kt
@@ -1,25 +1,26 @@
 package lambdas
 
 fun main(args: Array<String>) {
-lambda()
+    lambda()
 
-    val list = mapIntList(listOf(3,4,5,6), {index -> index + 5})
+    val list = mapIntList(listOf(3, 4, 5, 6), { index -> index + 5 })
     println(list)
 
     applyMapFilterReduce(listOf(2, 68, 32, 3, 15, 4, 43))
 
 }
 
-fun lambda(){
+fun lambda() {
+    //define a lambda var
     var sum: (Int, Int) -> Int
 
-    sum = {v1, v2 -> v1 + v2}
+    sum = { v1, v2 -> v1 + v2 }
 
     println(sum(3, 4))
 }
 
-
-fun mapIntList(intList: List<Int>, transform: (Int) -> Int): List<Int>{
+//transforms IntList values
+fun mapIntList(intList: List<Int>, transform: (Int) -> Int): List<Int> {
     val mutableList = mutableListOf<Int>()
 
     for (i in intList)
@@ -28,12 +29,12 @@ fun mapIntList(intList: List<Int>, transform: (Int) -> Int): List<Int>{
     return mutableList.toList()
 }
 
-fun applyMapFilterReduce(intList: List<Int>){
+fun applyMapFilterReduce(intList: List<Int>) {
     val sumOfIncrementedEvenValues =
             intList
-            .map { it + 1 }                       //increment all values of the array
-            .filter {it.rem(2) == 0 }        //get only even values
-            .reduce { acc, i -> acc + i }  // sum all values
+                    .map { it + 1 }                       //increment all values of the array
+                    .filter { it.rem(2) == 0 }        //get only even values
+                    .reduce { acc, i -> acc + i }  // sum all values
 
     println(sumOfIncrementedEvenValues)
 }
