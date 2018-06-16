10cc9aaaaa9e6ab3e1095a7c9e1443aef01a14e8
GianniGM <giangraziano@gmail.com>
2018-06-16 19:21:45 +0200

    Exercise 1 tracks updated

--- src/exercises/Exercise1.kt
+++ src/exercises/Exercise1.kt
@@ -4,29 +4,32 @@ import kotlin.test.assertEquals
 
 const val SOLUTION = "CUACCUUGAACUGAUGCAUUUAA"
 
-fun main(args: Array<String>) {
-    /*
-     * http://exercism.io/exercises/kotlin/rna-transcription/readme
-     * http://rosalind.info/problems/rna/
-     *
-     * Given a DNA strand, return its RNA complement (per RNA transcription).
-     *
-     * Both DNA and RNA strands are a sequence of nucleotides.
-     * The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G) and thymine (T).
-     *
-     *   Given a DNA strand, its transcribed RNA strand is formed by
-     *   replacing each nucleotide with its complement:
-     *
-     *   G -> C
-     *   C -> G
-     *   T -> A
-     *   A -> U
-     */
+/*
+* http://exercism.io/exercises/kotlin/rna-transcription/readme
+* http://rosalind.info/problems/rna/
+*
+* Given a DNA strand, return its RNA complement (per RNA transcription).
+*
+* Both DNA and RNA strands are a sequence of nucleotides.
+* The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G) and thymine (T).
+*
+*   Given a DNA strand, its transcribed RNA strand is formed by
+*   replacing each nucleotide with its complement:
+*
+*   G -> C
+*   C -> G
+*   T -> A
+*   A -> U
+*/
 
+// Write a function "complement" that does what described over here
+
+
+// Do not change anything from this point on
+fun main(args: Array<String>) {
     val dna = "GATGGAACTTGACTACGTAAATT"
 
-    val rna = "TODO"
-
+    val rna = dna
     assertEquals(SOLUTION, rna)
 }
 
